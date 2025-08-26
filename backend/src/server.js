4d20b7b8efe9b2f572ae79d2992a337e43854cf2
Sxhildeep <sahildeepgta@gmail.com>
2025-08-26 12:41:18 +0930

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,19 +5,33 @@ import dotenv from "dotenv"
 import rateLimiter from '../middleware/rateLimiter.js';
 import cors from "cors";
 
+import path from "path"
 
 dotenv.config();// db setup
 
 const app = express();
 const port = process.env.PORT;
-
-
-app.use(cors()); // to fix cors error
+const __dirname = path.resolve()
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin:"http://localhost:5173"
+    }));
+}
+ // to fix cors error
 app.use(express.json()); // access client side json data
 app.use(rateLimiter); // custom middleware
 
 
-app.use("/api/notes", notesRouter) // using our routes
+app.use("/api/notes", notesRouter); // using our routes
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=> {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
+
 
 connectDB().then(()=> { // connect to db then start server
     app.listen(port,()=> {
